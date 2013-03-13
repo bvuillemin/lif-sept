00c36a671ace3d5e880c68a7e9ace1d441679755
christelle.bodard@gmail.com <christelle.bodard@gmail.com@af7b8f54-17bd-f620-bca0-76c1c20c2efb>
2013-03-13 18:44:48 +0000

    modification de niveau.c et niveau.h

---
+++ src/niveau.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "unite.h"
+#include "niveau.h"
+
+void initilalise_niveau(Niveau *niveau, int niv, int exp)
+{
+	niveau = (*Niveau) malloc (sizeof(Niveau));
+	niveau -> niveau_unite = niv;
+	niveau -> experience = exp;
+}
+
+
+
+void set_niveau_unite(Niveau * niveau, int niv)
+{
+	niveau->niveau_unite=niv;
+}
+int get_niveau_unite(const Niveau * niveau)
+{
+	return niveau->niveau_unite;
+}
+
+void set_experience(Niveau niveau, int exp)
+{
+	niveau->experience=exp;
+}
+int get_experience(const Niveau * niveau)
+{
+	return niveau->experience;
+}
+
+void augmenter_niveau(Niveau niveau){}
+void augmenter_experience(Niveau niveau){}
+
+void libere_niveau(Niveau *niveau)
+{
+	niveau->niveau_unite=0;
+	niveau->experience=0;
+}
+void detruit_niveau(Niveau **niveau)
+{
+	libere_niveau(*niveau);
+	free(*niveau);
+	*niveau=NULL;
+}
+
