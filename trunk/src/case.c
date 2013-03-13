156ddd4677cd65db4454d6e7fee30f3adc2e7064
pierre.reynaud.1993@gmail.com <pierre.reynaud.1993@gmail.com@af7b8f54-17bd-f620-bca0-76c1c20c2efb>
2013-03-13 17:01:57 +0000

    maj

--- trunk/src/case.c
+++ trunk/src/case.c
@@ -2,22 +2,22 @@
 #include <stdio.h>
 #include "case.h"
 
-void set_x(case case_terrain, int x)
+void set_x(Case_terrain case_terrain, int x)
 {
     case_terrain.x = x;
 }
 
-int get_x(case case_terrain)
+int get_x(Case_terrain case_terrain)
 {
     return case_terrain.x;
 }
 
-void set_y(case case_terrain, int y)
+void set_y(Case_terrain case_terrain, int y)
 {
     case_terrain.y = y;
 }
 
-int get_y(case case_terrain)
+int get_y(Case_terrain case_terrain)
 {
     return case_terrain.y;
 }
