d90bbe0bda76f304816af79c4dbb9b12723f436e
pierre.reynaud.1993@gmail.com <PReynaud@users.noreply.github.com>
2013-03-13 18:49:12 +0000

    ajout module planete

---
+++ src/planete.h
@@ -0,0 +1,39 @@
+#ifndef _PLANETE_H_
+#define _PLANETE_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * \file      Module terrain
+ * \author    Les queues de cheval
+ * \version   0.1
+ * \date      13 mars 2013
+ * \brief     Planète et ses particularités
+ *
+ * \details
+ */
+
+ typedef struct
+ {
+     int id_planete;
+     char *nom_planete;
+     //lien sur proprietaire
+     //tab de batiments
+     //ressources produites
+     int taille_utilisee;
+     int taille_planete;
+     int planete_principale; //1 si oui, 0 sinon
+     int planete_colonise; //1 si oui, 0 sinon
+
+ }Planete;
+
+void initialise_planete(Planete *planete, int id);
+Planete *creer_planete(int id);
+
+
+void set_nom_planete(Planete *planete, char nom[30]);
+
+
+
+#endif
--- src/terrain.h
+++ src/terrain.h
@@ -23,18 +23,17 @@ typedef struct
     Case_terrain *tab_terrain;
 }Terrain;
 
-
-void set_taille_x(Terrain terrain_jeu, int x);
-int get_taille_x(Terrain terrain_jeu);
-
-void set_taille_y(Terrain terrain_jeu, int y);
-int get_taille_y(Terrain terrain_jeu);
-
 void initilalise_terrain(Terrain *terrain_jeu, int taille_x, int taille_y);
 Terrain *creer_terrain(int taille_x, int taille_y);
 
 void libere_terrain(Terrain *terrain_jeu);
 void detruit_terrain(Terrain **terrain_jeu);
+
+void set_taille_x(Terrain *terrain_jeu, int x);
+int get_taille_x(const Terrain *terrain_jeu);
+
+void set_taille_y(Terrain *terrain_jeu, int y);
+int get_taille_y(const Terrain *terrain_jeu);
 
 
 
