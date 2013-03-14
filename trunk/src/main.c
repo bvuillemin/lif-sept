3d087ede6b73bf837b7be5c7bd57e84a46b61a8a
pierre.reynaud.1993@gmail.com <pierre.reynaud.1993@gmail.com@af7b8f54-17bd-f620-bca0-76c1c20c2efb>
2013-03-14 23:48:24 +0000

    ajout d'un makefile opérationnel

---
+++ trunk/src/main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "planete.h"
+#include "case.h"
+#include "niveau.h"
+#include "unite.h"
+#include "terrain.h"
+
+int main()
+{
+    test_module_planete();
+    return 0;
+}
--- trunk/src/planete.c
+++ trunk/src/planete.c
@@ -4,13 +4,15 @@
 #include "planete.h"
 
 
+
+
 void initialise_planete(Planete *planete, int id)
 {
     planete->id_planete = id;
     planete->nom_planete = (char*)malloc(sizeof(char) * 30);
     planete->planete_colonise = 0;
     planete->planete_principale = 0;
-    planete->taille_planete = 0; // a modifier
+    planete->taille_planete = 0; /* a modifier*/
     planete->taille_utilisee = 0;
 }
 
@@ -31,3 +33,18 @@ void set_nom_planete(Planete *planete, char nom[30])
     }
 }
 
+void test_module_planete()
+{
+    Planete *terre;
+    printf("Verif de la création de planètes\n");
+    terre = creer_planete(1);
+    if((terre->id_planete == 1) && (terre->taille_planete == 0))
+    {
+		printf ("OK \n");
+	}
+	else
+	{
+		printf ("Echec\n");
+	}
+
+}
