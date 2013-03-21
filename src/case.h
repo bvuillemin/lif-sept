1fd6778cae422da64f31d70adf4baf78efd784bb
pierre.reynaud.1993@gmail.com <PReynaud@users.noreply.github.com>
2013-03-21 16:42:22 +0000

    Maj terrain: mais bug en cours...

--- src/case.h
+++ src/case.h
@@ -18,22 +18,26 @@
 typedef struct
 {
     int x,y;
-    char type_case; // Quel type mettre?
-    int presence_unite;//plutôt un booleen non ?
+    char type_case;// E pour l'espace, P pour une planète, N si non initialisé
+    bool presence_unite;
+	Planete *planete;//lien sur une planète si la case est de type planète, sinon NULL
 }Case_terrain;
 
-void initialise_case(Case_terrain *case_terrain);
+void initialise_case(Case_terrain *une_case_terrain);
 Case_terrain *creer_case();
 
-void set_x(Case_terrain *case_terrain, int x);
-int get_x(const Case_terrain *case_terrain);
+void set_x(Case_terrain *une_case_terrain, int x);
+int get_x(const Case_terrain *une_case_terrain);
 
-void set_y(Case_terrain *case_terrain, int y);
-int get_y(const Case_terrain *case_terrain);
+void set_y(Case_terrain *une_case_terrain, int y);
+int get_y(const Case_terrain *une_case_terrain);
 
-void definir_case_terrain(Case_terrain *case_terrain, char c);
+void detruit_case_terrain(Terrain **une_case_terrain);
+void libere_case_terrain(Case_terrain *une_case_terrain);
 
-void affiche_case_terrain(const Case_terrain *case_terrain);
+void definir_case_terrain(Case_terrain *une_case_terrain, char c);
+
+void affiche_case_terrain(const Case_terrain *une_case_terrain);
 void test_module_case_terrain();
 
 
