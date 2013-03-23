05fbad64cd4956b3f26ea8c4bdac61811d48be39
christelle.bodard@gmail.com <christelle.bodard@gmail.com@af7b8f54-17bd-f620-bca0-76c1c20c2efb>
2013-03-23 13:51:29 +0000

    modification des noms des variables(danc main.c, case.h/.c , terrain.h/c) et des fichiers (case et terrain) modification du makefile

--- src/main.c
+++ src/main.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 #include "planete.h"
-#include "case.h"
+#include "case_terrain_espace.h"
 #include "niveau.h"
 #include "unite.h"
-#include "terrain.h"
+#include "terrain_espace.h"
 #include "flotte.h"
 
 int main()
@@ -16,17 +16,18 @@ int main()
 	test_module_case_terrain();
 	test_module_terrain();*/
 
-	Terrain *un_terrain;
-    un_terrain = creer_terrain(20, 20);
-    modification_terrain(un_terrain, 'E');
-    affiche_terrain(un_terrain);
+	Terrain_espace *un_terrain_espace;
+    un_terrain_espace = creer_terrain_espace(20, 20);
+    modification_terrain_espace(un_terrain_espace, 'E');
+    affiche_terrain_espace(un_terrain_espace);
 
-    ajouter_planete_terrain(un_terrain, 2, 1);
-    affiche_terrain(un_terrain);
+    ajouter_planete_terrain_espace(un_terrain_espace, 2, 1);
+    affiche_terrain_espace(un_terrain_espace);
 
-    affiche_planete(un_terrain->tab_terrain[2*(un_terrain->taille_x)+1].planete);
+    affiche_planete(un_terrain_espace->tab_terrain_espace[2*(un_terrain_espace->taille_espace_x)+1].planete);
 
-    detruit_terrain(&un_terrain);
+    detruit_terrain_espace(&un_terrain_espace);
 
     return 0;
 }
+
---
+++ src/terrain_espace.c
@@ -0,0 +1,124 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "case_terrain_espace.h"
+#include "terrain_espace.h"
+
+void set_taille_espace_x(Terrain_espace *terrain_jeu_espace, int x)
+{
+    terrain_jeu_espace->taille_espace_x = x;
+}
+
+int get_taille_espace_x(const Terrain_espace *terrain_jeu_espace)
+{
+    return terrain_jeu_espace->taille_espace_x;
+}
+
+void set_taille_espace_y(Terrain_espace *terrain_jeu_espace, int y)
+{
+    terrain_jeu_espace->taille_espace_y = y;
+}
+
+int get_taille_espace_y(const Terrain_espace *terrain_jeu_espace)
+{
+    return terrain_jeu_espace->taille_espace_y;
+}
+
+Case_terrain_espace get_case_terrain_espace(const Terrain_espace *terrain_espace, const int x, const int y)
+{
+    return terrain_espace->tab_terrain_espace[x*(terrain_espace->taille_espace_x)+y];
+    /*return terrain_espace->tab_terrain_espace[y*(terrain_espace->taille_espace_y)+x];*/
+}
+
+void set_case_terrain_espace(const Terrain_espace *terrain_espace, int x, int y, char c)
+{
+    terrain_espace->tab_terrain_espace[y*(terrain_espace->taille_espace_y)+x].type_case_terrain_espace = c;
+}
+
+
+void initilalise_terrain_espace(Terrain_espace *terrain_jeu_espace, int taille_espace_x, int taille_espace_y)
+{
+    terrain_jeu_espace->taille_espace_x = taille_espace_x;
+    terrain_jeu_espace->taille_espace_y = taille_espace_y;
+    terrain_jeu_espace->tab_terrain_espace = (Case_terrain_espace *)malloc(sizeof(Case_terrain_espace)*taille_espace_x*taille_espace_y);
+
+}
+
+Terrain_espace *creer_terrain_espace(int taille_espace_x, int taille_espace_y)
+{
+    Terrain_espace *terrain_espace = (Terrain_espace *)malloc(sizeof(Terrain_espace));
+    initilalise_terrain_espace(terrain_espace, taille_espace_x, taille_espace_y);
+    return terrain_espace;
+}
+
+void libere_terrain_espace(Terrain_espace *terrain_jeu_espace)
+{
+    free(terrain_jeu_espace->tab_terrain_espace);
+    terrain_jeu_espace->taille_espace_x=0;
+    terrain_jeu_espace->taille_espace_y=0;
+}
+
+void detruit_terrain_espace(Terrain_espace **terrain_jeu_espace)
+{
+    libere_terrain_espace(*terrain_jeu_espace);
+    free(*terrain_jeu_espace);
+    *terrain_jeu_espace = NULL;
+}
+
+
+
+void affiche_terrain_espace(const Terrain_espace *terrain_espace)
+{
+    int i, j;
+    Case_terrain_espace unecase;
+    for(i=0;i<terrain_espace->taille_espace_x;i++)
+    {
+        for(j=0;j<terrain_espace->taille_espace_y;j++)
+        {
+            unecase = get_case_terrain_espace(terrain_espace, i, j);
+            printf("|%c|", unecase.type_case_terrain_espace);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
+void modification_terrain_espace(const Terrain_espace *terrain_espace, const char c)
+{
+    int i, j;
+    for(i=0;i<terrain_espace->taille_espace_x;i++)
+    {
+        for(j=0;j<terrain_espace->taille_espace_y;j++)
+        {
+            set_case_terrain_espace(terrain_espace, i, j, c);
+        }
+    }
+}
+
+void ajouter_planete_terrain_espace(Terrain_espace *terrain_espace, int x, int y)
+{
+    Planete *terre;
+    terre = creer_planete();
+    ajouter_planete(&(terrain_espace->tab_terrain_espace[x*(terrain_espace->taille_espace_x)+y]), terre);
+}
+
+
+/*void test_module_terrain_espace()
+{
+    Terrain_espace_espace *terrain_espace;
+    terrain_espace = creer_terrain(20, 20);
+    modification_terrain(terrain_espace, 'E');
+    affiche_terrain(terrain_espace);
+
+    ajouter_planete_terrain(terrain_espace, 4, 2);
+    affiche_terrain(terrain_espace);
+
+    affiche_planete(terrain_espace->tab_terrain_espace[2*(terrain_espace->taille_espace_x)+1].planete);
+
+    detruit_terrain_espace(&terrain_espace);
+
+}
+
+*/
+
