220479fdbafbadd98573cb5ecf2173b0b86dabd0
benoit.vuil@gmail.com <benoit.vuil@gmail.com@af7b8f54-17bd-f620-bca0-76c1c20c2efb>
2013-03-29 15:58:53 +0000

    modification des sauvegardes

--- trunk/src/sauvegarde.h
+++ trunk/src/sauvegarde.h
@@ -7,11 +7,11 @@
 
 #include "terrain_espace.h"
 
-void detruire_ancienne_sauvegarde(const char nom[30]);
+void detruire_sauvegarde(const char nom[30]);
 void sauvegarde_terrain(const Terrain_espace *un_terrain_espace, const char nom[30]);
 void sauvegarde_planete(const Planete *une_planete, const char nom[30]);
 void sauvegarde_flotte(const Flotte *une_flotte, const char nom[30]);
 void sauvegarde_unite(const Unite *une_unite, const char nom[30]);
-
+void ouverture_terrain(const char nom[30]);
 
 #endif
