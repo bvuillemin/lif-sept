381b9b89370393769fd22a344a65dadfffdef1b9
benoit.vuil@gmail.com <benoit.vuil@gmail.com@af7b8f54-17bd-f620-bca0-76c1c20c2efb>
2013-04-01 10:52:24 +0000

    Mise à jour des sauvegardes

--- src/sauvegarde.h
+++ src/sauvegarde.h
@@ -12,6 +12,7 @@ void sauvegarde_terrain(const Terrain_espace *un_terrain_espace, const char nom[
 void sauvegarde_planete(const Planete *une_planete, const char nom[30]);
 void sauvegarde_flotte(const Flotte *une_flotte, const char nom[30]);
 void sauvegarde_unite(const Unite *une_unite, const char nom[30]);
-void ouverture_terrain(const char nom[30]);
+void ouverture_terrain(FILE *f, long a);
+void selection_ouverture(const char nom[30], long a);
 
 #endif
