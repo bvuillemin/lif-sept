61e47451d246a429c3744930bae9ff10bf688834
benoit.vuil@gmail.com <benoit.vuil@gmail.com@af7b8f54-17bd-f620-bca0-76c1c20c2efb>
2013-04-08 13:24:47 +0000

    Grosse mise à jour des sauvegardes (+ le main pour les tester)

--- src/sauvegarde.h
+++ src/sauvegarde.h
@@ -12,7 +12,9 @@ void sauvegarde_terrain(const Terrain_espace *un_terrain_espace, const char nom[
 void sauvegarde_planete(const Planete *une_planete, const char nom[30]);
 void sauvegarde_flotte(const Flotte *une_flotte, const char nom[30]);
 void sauvegarde_unite(const Unite *une_unite, const char nom[30]);
-void ouverture_terrain(FILE *f, long a);
-void selection_ouverture(const char nom[30], long a);
+Terrain_espace* ouverture_terrain(FILE *f, long a);
+Planete* ouverture_planete(FILE *f, long a);
+Flotte* ouverture_flotte(FILE *f, long a);
+Terrain_espace* selection_ouverture(const char nom[30], long a);
 
 #endif
