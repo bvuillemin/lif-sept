e1321a4ffbe2d4fd8cd0c233026f2105ca55b7f4
benoit.vuil@gmail.com <benoit.vuil@gmail.com@af7b8f54-17bd-f620-bca0-76c1c20c2efb>
2013-04-09 08:54:03 +0000

    Sauvegarde fonctionnelle de planètes, flottes et d'un terrain

--- src/mainBen.c
+++ src/mainBen.c
@@ -17,11 +17,36 @@
 int main()
 {
     Terrain_espace *un_terrain_espace;
-    Terrain_espace *terrain_ouvert;
+    Sauvegarde *une_sauvegarde;
+    Flotte *une_flotte;
+    Planete *une_planete;
+    int a, b;
+    detruire_sauvegarde("test");
     un_terrain_espace = creer_terrain_espace(10,10);
     modification_terrain_espace(un_terrain_espace, 'E');
+    une_flotte = creer_flotte();
+    set_taille_maximum_flotte(une_flotte, 5);
+    set_taille_flotte(une_flotte, 4);
     affiche_terrain_espace(un_terrain_espace);
-    terrain_ouvert = selection_ouverture("test", 0);
-    affiche_terrain_espace(terrain_ouvert);
+    une_planete = creer_planete("Terre");
+    set_planete_colonisee(une_planete, 1);
+    set_metal(une_planete, 35);
+    set_population(une_planete, 56578);
+    set_position_planete(une_planete, 3, 5);
+    set_taille_planete(une_planete, 3);
+    set_taille_utilisee(une_planete, 5);
+    afficher_planete(une_planete);
+    sauvegarde_terrain(un_terrain_espace, "test");
+    sauvegarde_flotte(une_flotte, "test");
+    sauvegarde_planete(une_planete, "test");
+    une_sauvegarde = selection_ouverture("test", 0);
+    affiche_terrain_espace(une_sauvegarde->terrain_espace);
+    a = get_taille_espace_x(une_sauvegarde->terrain_espace);
+    b = get_taille_espace_x(une_sauvegarde->terrain_espace);
+    printf("%d, %d \n", a, b);
+    afficher_flotte(&une_sauvegarde->tab_flotte[0]);
+    printf("%d \n", une_sauvegarde->nb_flotte);
+    afficher_planete(&une_sauvegarde->tab_planete[0]);
+    printf("%d \n", une_sauvegarde->nb_planete);
 	return 0;
 }
--- src/sauvegarde.h
+++ src/sauvegarde.h
@@ -7,14 +7,31 @@
 
 #include "terrain_espace.h"
 
+typedef struct
+{
+    int nb_terrain_espace;
+    Terrain_espace* terrain_espace;
+    int nb_planete;
+    Planete* tab_planete;
+    int nb_flotte;
+    Flotte* tab_flotte;
+}Sauvegarde;
+
+void initialise_sauvegarde(Sauvegarde *une_sauvegarde);
+Sauvegarde *creer_sauvegarde();
+void ajouter_terrain_espace_sauvegarde(Sauvegarde *une_sauvegarde, Terrain_espace *un_terrain_espace);
+void ajouter_planete_sauvegarde(Sauvegarde *une_sauvegarde, Planete *une_planete);
+void ajouter_flotte_sauvegarde(Sauvegarde *une_sauvegarde, Flotte *une_flotte);
+
 void detruire_sauvegarde(const char nom[30]);
 void sauvegarde_terrain(const Terrain_espace *un_terrain_espace, const char nom[30]);
 void sauvegarde_planete(const Planete *une_planete, const char nom[30]);
 void sauvegarde_flotte(const Flotte *une_flotte, const char nom[30]);
-void sauvegarde_unite(const Unite *une_unite, const char nom[30]);
+void sauvegarde_unite(const Unite *une_unite, FILE* f);
 Terrain_espace* ouverture_terrain(FILE *f, long a);
 Planete* ouverture_planete(FILE *f, long a);
 Flotte* ouverture_flotte(FILE *f, long a);
-Terrain_espace* selection_ouverture(const char nom[30], long a);
+Unite* ouverture_unite(FILE *f);
+Sauvegarde* selection_ouverture(const char nom[30], long a);
 
 #endif
