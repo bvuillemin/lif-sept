f666794c9d27f6e7be719b3532ba967eeffc4439
pierre.reynaud.1993@gmail.com <pierre.reynaud.1993@gmail.com@af7b8f54-17bd-f620-bca0-76c1c20c2efb>
2013-04-16 09:59:39 +0000

    ajout de l'affichage des polices d'ecriture pour ressources

--- trunk/src/affichage.c
+++ trunk/src/affichage.c
@@ -5,10 +5,39 @@
 #include "SDL_image.h"
 #else
 #include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
 #endif
+
+#include "jeu.h"
 #include "constante.h"
 #include "terrain_espace.h"
 #include "terrain_combat.h"
+
+
+SDL_Surface* affichage_ressource(Jeu *un_jeu, SDL_Surface *surface_ressource)
+{
+    SDL_Surface *texte;
+    SDL_Rect position;
+    TTF_Font *police = NULL;
+    char ressource[200] = "";
+
+    TTF_Init();
+    police = TTF_OpenFont("space_age.ttf", 14);
+    SDL_Color couleur_blanche = {255, 255, 255};
+
+    sprintf(ressource, "Ressources:  Metal:%d  Argent:%d  Carburant:%d  Population:%d", get_metal_joueur(&un_jeu->tab_joueur[0]), get_argent_joueur(&un_jeu->tab_joueur[0]), get_carburant_joueur(&un_jeu->tab_joueur[0]), get_population_joueur(&un_jeu->tab_joueur[0]));
+    texte = TTF_RenderText_Blended(police, ressource, couleur_blanche);
+    position.x = 10;
+    position.y = 2;
+    SDL_BlitSurface(texte, NULL, surface_ressource, &position);
+    return surface_ressource;
+
+    SDL_FreeSurface(texte);
+    TTF_CloseFont(police);
+    TTF_Quit();
+}
+
+
 SDL_Surface* creer_affichage_terrain(Terrain_espace *un_terrain_espace)
 {
 	SDL_Surface *carte = NULL;
@@ -19,8 +48,8 @@ SDL_Surface* creer_affichage_terrain(Terrain_espace *un_terrain_espace)
 	SDL_Rect position;
 	SDL_Surface *image_une_case = NULL;
     SDL_Surface *fond = NULL;
-	int i, j;/*
-	Case_terrain_espace *une_case;*/
+	int i, j;
+	Case_terrain_espace *une_case;
 
 	position_planete.x = 0;
 	position_planete.y = 0;
@@ -41,16 +70,21 @@ SDL_Surface* creer_affichage_terrain(Terrain_espace *un_terrain_espace)
 		{
 			position.x = i * 100;
 			position.y = j *100;
+			une_case = get_case_terrain_espace(un_terrain_espace, i, j);
+			if(une_case->type_case_terrain_espace == 'P')
+			{
+			    position_planete.x = (i * 100);
+                position_planete.y = (j * 100);
+                SDL_BlitSurface(planete, NULL, carte, &position_planete);
+			}
 			SDL_BlitSurface(image_une_case, NULL, carte, &position);
 		}
 	}
-	position_planete.x = (2 * 100);
-	position_planete.y = (1 * 100);
-	SDL_BlitSurface(planete, NULL, carte, &position_planete);
 
-	position_planete2.x = (8 * 100);
-	position_planete2.y = (8 * 100);
-	SDL_BlitSurface(planete2, NULL, carte, &position_planete2);
+    SDL_FreeSurface(planete);
+    SDL_FreeSurface(planete2);
+    SDL_FreeSurface(image_une_case);
+    SDL_FreeSurface(fond);
 
 	return carte;
 }
@@ -71,11 +105,11 @@ SDL_Surface* affichage_terrain(Terrain_espace *un_terrain_espace, SDL_Surface *c
 
 	/*SDL_FillRect(resultat, &position, SDL_MapRGB(resultat->format, 255, 255, 255));*/
 	SDL_BlitSurface(carte, &affichage, resultat, &position);
-	
+
 	return resultat;
 }
 
-void affichage_ecran(Terrain_espace *un_terrain_espace)
+void affichage_ecran(Jeu *un_jeu, Terrain_espace *un_terrain_espace)
 {
 	SDL_Surface *ecran = NULL;
 	SDL_Surface *carte = NULL;
@@ -96,21 +130,22 @@ void affichage_ecran(Terrain_espace *un_terrain_espace)
 
     position3.x = 0;
 	position3.y = 20;
-    
+
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_EnableKeyRepeat(10, 10);
 	ecran = SDL_SetVideoMode(1280, 720, 32, SDL_SWSURFACE);
 	interface = IMG_Load("interface.png");
     SDL_BlitSurface(interface, NULL, ecran, &position3);
-	
+
 	ressource = IMG_Load("ressource.png");
+	ressource = affichage_ressource(un_jeu, ressource);
 	SDL_BlitSurface(ressource, NULL, ecran, &position2);
 
 	carte = creer_affichage_terrain(un_terrain_espace);
 	deplacement_carte = SDL_CreateRGBSurface(SDL_SWSURFACE, 1080, 600, 32, 0, 0, 0, 0);
 	deplacement_carte = affichage_terrain(un_terrain_espace, carte, deplacement_carte);
 	SDL_BlitSurface(deplacement_carte, NULL, ecran, &position1);
-	
+
 	SDL_Flip(ecran);
 
 	while (continuer)
@@ -158,6 +193,12 @@ void affichage_ecran(Terrain_espace *un_terrain_espace)
 			break;
 		}
 	}
+
+	SDL_FreeSurface(interface);
+	SDL_FreeSurface(ressource);
+	SDL_FreeSurface(carte);
+	SDL_FreeSurface(deplacement_carte);
+	SDL_FreeSurface(ecran);
 	SDL_Quit();
 }
 
@@ -165,7 +206,7 @@ void pause()
 {
     int continuer = 1;
     SDL_Event event;
-  
+
     while (continuer)
     {
         SDL_WaitEvent(&event);
@@ -179,7 +220,7 @@ void pause()
 
 void affichage_ecran_combat(Terrain_combat *un_terrain_combat)
 {
-	
+
 	 if (SDL_Init(SDL_INIT_VIDEO) == -1) /*Démarrage de la SDL. Si erreur :*/
     {
         fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError()); /* Écriture de l'erreur*/
