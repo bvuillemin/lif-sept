b4e28d608d387077742d4d3e483a615ae49d8f61
pierre.reynaud.1993@gmail.com <pierre.reynaud.1993@gmail.com@af7b8f54-17bd-f620-bca0-76c1c20c2efb>
2013-05-01 20:10:50 +0000

    Correstion de bugs, ajout de batiments, ajout d'une minimap (qui fonctionne quasimment)

--- trunk/src/batiment.h
+++ trunk/src/batiment.h
@@ -8,12 +8,27 @@
 
 #define NB_TOUR_BATIMENT_QUARTIER_GENERAL 1
 #define NB_TOUR_BATIMENT_METAL 1
+#define NB_TOUR_BATIMENT_ARGENT 1
+#define NB_TOUR_BATIMENT_CARBURANT 1
+#define NB_TOUR_BATIMENT_POPULATION 1
+#define NB_TOUR_BATIMENT_SPATIOPORT 1
 
-
-void validation_creer_batiment_metal(Planete *une_planete);
 void validation_creer_batiment_quartier_general(Planete *une_planete);
 void creer_batiment_quartier_general(Planete *une_planete);
+
+void validation_creer_batiment_metal(Planete *une_planete);
 void creer_batiment_metal(Planete *une_planete);
 
+void validation_creer_batiment_argent(Planete *une_planete);
+void creer_batiment_argent(Planete *une_planete);
+
+void validation_creer_batiment_carburant(Planete *une_planete);
+void creer_batiment_carburant(Planete *une_planete);
+
+void validation_creer_batiment_population(Planete *une_planete);
+void creer_batiment_population(Planete *une_planete);
+
+void validation_creer_batiment_spatioport(Planete *une_planete);
+void creer_batiment_spatioport(Planete *une_planete);
 
 #endif
