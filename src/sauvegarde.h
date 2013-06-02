f523dc87097f7e2b05dbf2ee2aa39605c43ac945
benoit.vuil@gmail.com <benoit.vuil@gmail.com@af7b8f54-17bd-f620-bca0-76c1c20c2efb>
2013-06-02 21:17:32 +0000

    Migration vers nouvelle_partie, résolutions de bugs...

--- src/sauvegarde.h
+++ src/sauvegarde.h
@@ -18,7 +18,7 @@ typedef struct
 void initialise_sauvegarde(Sauvegarde *une_sauvegarde);
 Sauvegarde *creer_sauvegarde();
 void creer_fichier_sauvegarde(const char nom[30], Terrain_espace *un_terrain_espace, Jeu *un_jeu);
-void detruire_sauvegarde(const char nom[30]);
+void detruire_sauvegarde(Sauvegarde **une_sauvegarde);
 Sauvegarde* selection_ouverture(const char nom[30]);
 
 #endif
