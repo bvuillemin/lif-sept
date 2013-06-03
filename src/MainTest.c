e772c4fe52dff3b079fbe391b7a0e7a3df9b0c88
benoit.vuil@gmail.com <benoit.vuil@gmail.com@af7b8f54-17bd-f620-bca0-76c1c20c2efb>
2013-06-03 10:23:35 +0000

    Test régression de la sauvegarde (et MainTest)

---
+++ src/MainTest.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "planete.h"
+#include "case_terrain_espace.h"
+#include "unite.h"
+#include "terrain_espace.h"
+#include "flotte.h"
+#include "sauvegarde.h"
+#include "terrain_combat.h"
+#include "case_terrain_combat.h"
+#include "joueur.h"
+#include "jeu.h"
+#include "batiment.h"
+#include "constante.h"
+#include "affichage.h"
+
+int main(int argc, char *argv[])
+{
+    testRegression_sauvegarde();
+    printf("Tests réussis");
+	return EXIT_SUCCESS;
+}
--- src/sauvegarde.h
+++ src/sauvegarde.h
@@ -20,5 +20,5 @@ Sauvegarde *creer_sauvegarde();
 void creer_fichier_sauvegarde(const char nom[30], Terrain_espace *un_terrain_espace, Jeu *un_jeu);
 void detruire_sauvegarde(Sauvegarde **une_sauvegarde);
 Sauvegarde* selection_ouverture(const char nom[30]);
-
+void testRegression_sauvegarde(void);
 #endif
